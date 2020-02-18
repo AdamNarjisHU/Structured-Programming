fa60f16146ca745fa8e2e8fb52be8b1f9a338f03
AdamNarjisHU <60732550+AdamNarjisHU@users.noreply.github.com>
2020-02-18 10:27:41 +0100

    Update MastermindPCcode.py

--- Mastermind/MastermindPCcode.py
+++ Mastermind/MastermindPCcode.py
@@ -1,52 +1,45 @@
 import random
 
+
 kleuren_lijst = ['rood', 'oranje', 'geel', 'groen', 'blauw', 'paars']
-
-
 def gok():
     k1 = input('Geef je 1e kleur: ')
     k2 = input('Geef je 2e kleur: ')
     k3 = input('Geef je 3e kleur: ')
     k4 = input('Geef je 4e kleur: ')
-    guess = [k1, k2, k3, k4]
+    guess = [k1.lower(), k2.lower(), k3.lower(), k4.lower()]
     print(guess)
     return guess
 
-def generate_secrete_key():
+def generate_secret_key():
     secret_key = []
     for kleur in range(4):
         kleur = random.choice(kleuren_lijst)
         secret_key.append(kleur)
-    print('antwoord: {}'.format(secret_key))
+    print('antwoord:', secret_key)
     return secret_key
 
-
 def mastermind_pc_code():
-    secret_key = generate_secrete_key()
+    secret_key = generate_secret_key()
     guess = gok()
     if guess == secret_key:
         print('Gefeliciteerd je bent een mastermind!')
     else:
         aantal_guesses = 0
         while guess != secret_key:
-            tips = []
-            aantal_guesses += 1
+            pins = []
+            pogingen += 1
             for i in range(0, 4):
                 if (guess[i] == secret_key[i]):
-                    tips.append('wit')
+                    pins.append('wit')
                     print('Gefeliciteerd je bent een mastermind!')
                 elif (guess[i] in secret_key):
-                    tips.append('zwart')
-
-
-            print(tips)
-            print('Je hebt {} van de 10 pogingen gebruikt'.format(aantal_guesses))
-
-            if aantal_guesses <= 9:
+                    pins.append('zwart')
+            print(pins)
+            print('Je hebt {} van de 10 pogingen gebruikt'.format(pogingen))
+            if pogingen < 10:
                 guess = gok()
             else:
                 print('game over')
                 break
-
-
 mastermind_pc_code()
